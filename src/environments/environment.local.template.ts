30a06e91dff8e0b7d18e23a911e6c6d9e8c09b78
OptrickMedia <tom@optrickmedia.com>
2025-10-26 14:46:02 -0400

    1ere Aventure d'angular
    petit projet qui met en place un boilerplate pour une app

---
+++ src/environments/environment.local.template.ts
@@ -0,0 +1,6 @@
+// Contenu de environment.local.template.ts
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  apiKey: 'METTRE_VOTRE_CLÉ_API_ICI' // Exemple
+};
